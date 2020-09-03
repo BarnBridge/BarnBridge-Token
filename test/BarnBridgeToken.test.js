83d18045cb9ae9e1f413c4b8677f2df16124e45f
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-09-03 12:31:45 +0300

    wip: erc20 token - update tests

--- test/BarnBridgeToken.test.js
+++ test/BarnBridgeToken.test.js
@@ -4,43 +4,32 @@ const {BigNumber} = require("bignumber.js");
 
 use(require('chai-bignumber')());
 
-
 describe("BarnBridgeToken", function() {
-  it('can deploy', async function() {
+  let token;
+
+  beforeEach(async function() {
     const Token = await ethers.getContractFactory("BarnBridgeToken")
-    const token = await Token.deploy()
+    token = await Token.deploy()
 
     await token.deployed(); 
+  })
 
+  it('can deploy', async function() {
     expect(token.address).to.not.equal(0)
   })
 
   it("Has correct name", async function() {
-    const Token = await ethers.getContractFactory("BarnBridgeToken")
-    const token = await Token.deploy()
-
-    await token.deployed(); 
-
     expect(await token.name()).to.equal("BarnBridge Governance Token")
   })
 
   it("Has correct symbol", async function() {
-    const Token = await ethers.getContractFactory("BarnBridgeToken")
-    const token = await Token.deploy()
-
-    await token.deployed(); 
-
     expect(await token.symbol()).to.equal("BOND")
   })
 
   it("Mints 10MM tokens", async function() {
-    const Token = await ethers.getContractFactory("BarnBridgeToken")
-    const token = await Token.deploy()
+    let actual = new BigNumber((await token.totalSupply()).toString())
+    let expected = new BigNumber('1e25');
 
-    await token.deployed(); 
-
-    let x = new BigNumber('10000000000000000000000000');
--
-    expect(await token.totalSupply()).to.be.bignumber.equal(x.toString())
+    expect(actual).to.be.bignumber.equal(expected)
   })
 });
